e46a5d36cd0483289c3f5ecd98069ee3166b5d46
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-13 22:35:14 +0300

    Initial Commit

---
+++ UITableViewDynamicCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewDynamicCells
+//
+//  Created by EnzoF on 13.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
