bed2c81ffcee60b04794a1efac98529f5edc54d9
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-13 23:35:17 +0300

    level Pupil completed

--- UITableViewDynamicCells/ViewController.m
+++ UITableViewDynamicCells/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource>
+
+@property (strong,nonatomic) NSMutableArray<UIColor*> *mArrayColors;
 
 @end
 
@@ -16,12 +18,67 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    for(int n = 0; n <= 999; n++)
+    {
+        [self.mArrayColors addObject:[self randomColor]];
+    }
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - lazy initialization
+
+-(NSMutableArray*)mArrayColors{
+    if(!_mArrayColors)
+    {
+        _mArrayColors = [[NSMutableArray alloc] init];
+    }
+    return _mArrayColors;
+}
+
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.mArrayColors count];
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *identifier = @"CellColor";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+
+    if(!cell)
+    {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
+    }
+    
+    UIColor *currentCellColor = [self.mArrayColors objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@" Ячейка - %ld",(long)indexPath.row];
+    
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    
+    [currentCellColor getRed:&red green:&green blue:&blue alpha:nil];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"RGB (%1.2f,%1.2f,%1.2f,1.f)",red,green,blue];
+    cell.textLabel.textColor = currentCellColor;
+    return cell;
+}
+
+
+#pragma mark - metods
+
+-(UIColor*)randomColor{
+
+    CGFloat red = (float)(arc4random() % 256) / 255.f;
+    CGFloat green = (float)(arc4random() % 256) / 255.f;
+    CGFloat blue = (float)(arc4random() % 256) / 255.f;
+    
+    return [[UIColor alloc]initWithRed:red green:green blue:blue alpha:1.f];
 }
 
 @end
